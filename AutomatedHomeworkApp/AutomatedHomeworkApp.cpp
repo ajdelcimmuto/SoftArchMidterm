b78e644bc47235d4384b5d211465ae30690def54
Nate Alter <nalter8na@gmail.com>
2022-03-01 14:19:15 -0500

    Added automated exe

---
+++ AutomatedHomeworkApp/AutomatedHomeworkApp.cpp
@@ -0,0 +1,25 @@
+// AutomatedHomeworkApp.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include "../Core/Core.h"
+
+int main()
+{
+    std::cout << "Automated Homework App\n" << std::endl;
+    performAction(2);
+    performAction(3);
+    performAction(4);
+    performAction(5);
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file

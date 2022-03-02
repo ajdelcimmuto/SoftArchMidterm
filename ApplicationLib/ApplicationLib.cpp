e377c95585f6739942cce3bcddfae6f00410d10f
ajdelcimmuto <ajdelco22@gmail.com>
2022-03-02 17:04:04 -0500

    adding translation work

--- ApplicationLib/ApplicationLib.cpp
+++ ApplicationLib/ApplicationLib.cpp
@@ -1,25 +1,42 @@
 // pch.cpp: source file corresponding to the pre-compiled header
-
 #include "ApplicationLib.h"
+#include <boost/locale.hpp>
+#include <iostream>
+
+using namespace std;
+using namespace boost::locale;
 
 // When you are using pre-compiled headers, this source file is necessary for compilation to succeed.
 APPLICATIONLIB_API void performAction(int choice) {
 
-	switch (choice)
-	{
-	case 2:
-		std::cout << GetLoginName() << std::endl << std::endl;
-		break;
-	case 3:
-		std::cout << GetAssignmentName() << std::endl << std::endl;
-		break;
-	case 4:
-		std::cout << GetCourseName() << std::endl << std::endl;
-		break;
-	case 5:
-		std::cout << GetUserInformationName() << std::endl << std::endl;
-		break;
-	default:
-		break;
-	}
+	generator gen;
+
+	// Specify location of dictionaries
+	gen.add_messages_path(".");
+	gen.add_messages_domain("hello");
+
+	// Generate locales and imbue them to iostream
+	locale::global(gen("de_DE.UTF - 8"));
+	// locale::global(gen(""));
+
+	cout.imbue(locale());
+	
+	cout << translate("Hello World") << endl;
+	//switch (choice)
+	//{
+	//case 2:
+	//	cout << translate(GetLoginName()) << endl << endl;
+	//	break;
+	//case 3:
+	//	cout << translate(GetAssignmentName()) << endl << endl;
+	//	break;
+	//case 4:
+	//	cout << translate(GetCourseName()) << endl << endl;
+	//	break;
+	//case 5:
+	//	cout << translate(GetUserInformationName()) << endl << endl;
+	//	break;
+	//default:
+	//	break;
+	//}
 };
